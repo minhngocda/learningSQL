6101027c926bba7eecb910bf0762c6a5e3351a45
Jenny Nguyen <ngoc.nm.hvhc@gmail.com>
2022-02-20 18:09:31 +0100

    New Company hackerrank solution

--- hackerrank/new_company.sql
+++ hackerrank/new_company.sql
@@ -1,4 +1,4 @@
--- https://www.hackerrank.com/challenges/the-company/problem?isFullScreen=true
+# https://www.hackerrank.com/challenges/the-company/problem?isFullScreen=true
 select distinct c.company_code, c.founder, count(distinct l.lead_manager_code), count(distinct s.senior_manager_code), count(distinct m.manager_code), count(distinct e.employee_code)
  from company c, lead_manager l, Senior_Manager s, Manager m , Employee e
  where c.company_code = l.company_code 
